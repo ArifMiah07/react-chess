4e9cdfce4dfa465f5ea39a8323c88742a9e0a4eb
ArifMiah07 <arifmiah.me322@gmail.com>
2025-01-28 18:08:39 +0600

    added routes and model

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { Button } from "./components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./components/ui/card";
 
 function App() {
   return (
     <div className="bg-green-500 p-5">
-      <h1 className="font-bold text-7xl text-red-400 text-center">
-        Tailwind CSS is working!
+      <h1 className="font-bold text-3xl text-white text-center">
+      React Chess
       </h1>
-      <div>
-        <Button>Click me</Button>
-      </div>
-      <div>
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-      </div>
     </div>
   );
 }
